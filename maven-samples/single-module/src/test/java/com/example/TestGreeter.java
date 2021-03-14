9595341b3049862104985cc51f6e90391fd76da1
sergiosanchezbarradas <sergio_sanchez_b@hotmail.com>
2021-03-14 18:40:47 +1100

    greeting messaged changed

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -32,7 +32,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage1() {
+  public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
