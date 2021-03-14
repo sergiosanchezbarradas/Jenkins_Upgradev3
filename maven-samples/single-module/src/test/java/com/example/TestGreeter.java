37f06040f96338772249d89f6c1c7ddd8aff2555
sergiosanchezbarradas <sergio_sanchez_b@hotmail.com>
2021-03-14 18:32:36 +1100

    new1

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,5 +36,11 @@ public class TestGreeter {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  
+    @Test
+    public void greetShouldIncludeGreetingMessage() {
+      String someone = "World, Give me the money! Devops";
+  
+      assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
